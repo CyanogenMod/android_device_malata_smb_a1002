3bcc0923d533989f35fd8aefc76dd95edc904cad
Brint E. Kriebel <github@bekit.net>
2010-12-22 00:20:59 -0800

    Add PRODUCT_PROPERTY_OVERRIDES for network location.

--- device_harmony.mk
+++ device_harmony.mk
@@ -54,6 +54,12 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
+# Set property overrides
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1 \
+    ro.setupwizard.enable_bypass=1
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
